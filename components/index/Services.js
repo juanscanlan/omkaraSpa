e161bdb0ca28af75053bd4913a6cd54c8c93a1a1
Juan Scanlan <juanscanlan@gmail.com>
2021-10-07 16:30:06 -0500

    Removed dynamic file name that was not required

--- components/index/Services.js
+++ components/index/Services.js
@@ -1,13 +1,18 @@
 import styles from "./services.module.scss";
 
+import Link from "next/link";
+
 function Services() {
   return (
     <div className={styles.container}>
       <h3 className={styles.container__title}>Servicios</h3>
       <div className={styles.services}>
-        <div className={`${styles.services__box} ${styles.fishSpa}`}>
-          Ictioterapia
-        </div>
+        <Link href="/Ictioterapia">
+          <div className={`${styles.services__box} ${styles.fishSpa}`}>
+            Ictioterapia
+          </div>
+        </Link>
+
         <div className={`${styles.services__box} ${styles.masajes}`}>
           Masajes
         </div>
---
+++ pages/Ictioterapia.js
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Home from "../components/index/Home";
+import Features from "../components/index/Features";
+import Services from "../components/index/Services";
+import CompanyInfo from "../components/CompanyInfo";
+import Footer from "../components/Footer";
+
+function Ictioterapia() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <CompanyInfo />
+      <Footer />
+    </div>
+  );
+}
+
+export default Ictioterapia;
