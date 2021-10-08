04017d0caf486490cb20807831acf0948a8eaa79
Juan Scanlan <juanscanlan@gmail.com>
2021-10-08 11:51:10 -0500

    Added features section

--- components/index/Features.js
+++ components/index/Features.js
@@ -1,11 +1,43 @@
 import styles from "./features.module.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChild } from "@fortawesome/free-solid-svg-icons";
+import { faHandshake, faGrin } from "@fortawesome/free-regular-svg-icons";
+import {
+  faFacebookSquare,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 function Features() {
   return (
     <div className={styles.features}>
-      <div className={styles.features__box}>Vision?</div>
-      <div className={styles.features__box}>Mision?</div>
-      <div className={styles.features__box}>Valores?</div>
+      <div className={styles.features__box}>
+        <FontAwesomeIcon icon={faGrin} className={styles.icon} />
+        <div className={styles.textContainer}>
+          <h3>Visión</h3>
+          <p>
+            Nuestra visión es... blah blah blah blah blah blah blah blah blah
+          </p>
+        </div>
+      </div>
+      <div className={styles.features__box}>
+        <FontAwesomeIcon icon={faChild} className={styles.icon} />
+        <div className={styles.textContainer}>
+          <h3>Misión</h3>
+          <p>
+            Nuestra misión es... blah blah blah blah blah blah blah blah blah
+          </p>
+        </div>
+      </div>
+      <div className={styles.features__box}>
+        <FontAwesomeIcon icon={faHandshake} className={styles.icon} />
+        <div className={styles.textContainer}>
+          <h3>Valores</h3>
+          <p>
+            Nuestros valores son... blah blah blah blah blah blah blah blah blah
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
